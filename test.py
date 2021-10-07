7d5e0eebcb2ec67868b0ccbdad46fbd45cf29e05
AmanPatwa <amanpatwa999@gmail.com>
2021-10-07 17:00:16 +0530

    Initial Changes

---
+++ test.py
@@ -0,0 +1,85 @@
+import json, os
+import boto3, zipfile
+s3_client = boto3.client('s3')
+glue_client = boto3.client('glue')
+
+def create_glue_job(fun_name):
+    base_func_name = fun_name.split('.')
+    base_func_name = base_func_name[0]
+    response = glue_client.create_job(
+        Name= base_func_name,
+        Role='arn:aws:iam::130159455024:role/SunLifeCyberSecurity-Developer-3857',
+        ExecutionProperty={
+            'MaxConcurrentRuns': 123
+        },
+        Command={
+            'Name': 'glueetl',
+            'ScriptLocation': 'string',
+            'PythonVersion': '3'
+        },
+        
+    )
+    return response
+
+def update_glue_job(fun_name):
+    response = glue_client.update_job(
+        JobName= fun_name,
+        JobUpdate={
+            'Role': 'arn:aws:iam::130159455024:role/SunLifeCyberSecurity-Developer-3857',
+            'ExecutionProperty': {
+                'MaxConcurrentRuns': 123
+            },
+            'Command': {
+                'Name': 'glueetl',
+                'ScriptLocation': 'string',
+                'PythonVersion': '3'
+            }
+        }
+    )
+    return response
+
+def get_glue_job(fun_name):
+    response = glue_client.get_job(
+        JobName=fun_name
+    )
+    return response
+
+def handler():
+    name1 = os.environ['name1']
+    file_name = name1.split(' ')
+    print(file_name)
+    allowed_files = ["sunlife-aman-glue-test.py","Sunlife_cyber_sec_test_1.py"]
+    for name in file_name:
+        if name in allowed_files:
+            # fun_name = name.split('.')
+            # fun_name = fun_name[0]
+            # print(fun_name)
+            try:
+                # zip_file = zipfile.ZipFile(fun_name+'.zip','w')
+                # zip_file.write(name,compress_type=zipfile.ZIP_DEFLATED)
+                # zip_file.close()
+                response = s3_client.upload_file(name, 'sunlife-cybersec-test', 'Glue/'+name)
+                # print(response)
+                
+
+            except Exception as e:
+                print("Error While uploading to s3")
+                return False
+
+            base_func_name = name.split('.')
+            base_func_name = base_func_name[0]
+            try:
+                res = get_glue_job(base_func_name)
+                if res:
+                    print("Updating boto3 job")
+                    response = update_glue_job(base_func_name)
+                    print(response)
+            except Exception as e:
+                print("Glue job not present")
+                print("Creating a new glue job")
+                response = create_glue_job(base_func_name)
+                print(response)
+
+
+            
+handler()
