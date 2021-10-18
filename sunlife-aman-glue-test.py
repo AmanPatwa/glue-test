1d2e98262383a65dc8e6fe35d9f5d78f2f081872
AmanPatwa <amanpatwa999@gmail.com>
2021-10-18 14:59:58 +0530

    test

--- sunlife-aman-glue-test.py
+++ sunlife-aman-glue-test.py
@@ -14,8 +14,8 @@ print(sc.getConf().getAll())
 glueContext = GlueContext(SparkContext.getOrCreate())
 
 
-datasource0 = glueContext.create_dynamic_frame.from_catalog(database = 'dbcrawler', table_name = 'sunlife-cyber-sec-testtestset_json')
-datasource0.printSchema()
+# datasource0 = glueContext.create_dynamic_frame.from_catalog(database = 'dbcrawler', table_name = 'sunlife-cyber-sec-testtestset_json')
+# datasource0.printSchema()
 ## @type: ApplyMapping
 ## @args: [mapping = [("date", "string", "date", "string"), ("serverid", "string", "serverid", "string"), ("guard_sender", "string", "guard_sender", "string"), ("ruleid", "string", "ruleid", "string"), ("ruledesc", "string", "ruledesc", "string"), ("severity", "string", "severity", "string"), ("devtime", "string", "devtime", "string"), ("servertype", "string", "servertype", "string"), ("classification", "string", "classification", "string"), ("category", "string", "category", "string"), ("dbprotocolversion", "string", "dbprotocolversion", "string"), ("usrname", "string", "usrname", "string"), ("sourceprogram", "string", "sourceprogram", "string"), ("start", "string", "start", "string"), ("dbuser", "string", "dbuser", "string"), ("dst", "string", "dst", "string"), ("dstport", "string", "dstport", "string"), ("src", "string", "src", "string"), ("srcport", "string", "srcport", "string"), ("protocol", "string", "protocol", "string"), ("type", "string", "type", "string"), ("violationid", "string", "violationid", "string"), ("sql", "string", "sql", "string"), ("error", "string", "error", "string"), ("other", "string", "other", "string")], transformation_ctx = "applymapping1"]
 ## @return: applymapping1
@@ -40,12 +40,12 @@ datasource0.printSchema()
 # txt = SQLContext.sql( "SELECT 1")
 # txt.show(2000000, False)
 
-# from pyspark.sql.functions import col
+from pyspark.sql.functions import col
 
 
-# def filter_spark_data_frame(
-#     dataframe,
-#     column_name='age',
-#     value=20,
-# ):
-#     return dataframe.where(col(column_name) > value)
+def filter_spark_data_frame(
+    dataframe,
+    column_name='age',
+    value=20,
+):
+    return dataframe.where(col(column_name) > value)
